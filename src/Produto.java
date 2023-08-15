9b0f343ecf1a0010d29aef1ad1f3b06ba89ce9de
junior366 <claudianosilvajunior373@gmail.com>
2023-08-15 08:23:21 -0300

    balonismo

---
+++ src/Produto.java
@@ -0,0 +1,27 @@
+public class Produto {
+    // Atributos
+    boolean diet;
+    private String nome;
+    double preco;
+
+    //Setter
+    public void setNome(String _nome){
+        this.nome = _nome;
+    }
+
+    //método construtor padrão
+    Produto(){}
+    // Sobrecarga de método
+    Produto(double _preco){
+
+        public Produto(String _nome,
+                           double _preco){
+
+            this.preco = _preco;
+            this.nome =  _nome;
+        }
+
+
+    }
+}
+
